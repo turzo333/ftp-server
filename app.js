3e429391739829390dcca6e1e1e32bc6073645d2
Saif Ahamed Turzo <saifturzo3@gmail.com>
2019-10-24 10:32:01 +0600

    initial push

---
+++ app.js
@@ -0,0 +1,24 @@
+var express  	= require('express');
+var ejs  		= require('ejs');
+var bodyParse  	= require('body-parser');
+var exSession  	= require('express-session');
+var cookieParser= require('cookie-parser');
+var home  		= require('./controllers/home');
+
+var app 		= express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParse.urlencoded({extended:false}));
+
+
+
+//ROUTING
+app.get('/', function(req, res){
+	res.send('<h2>hello from express</h2>');
+});
+
+
+//SERVER STARTUP
+app.listen(3000, function(){
+	console.log('server started at 3000...');
+});
